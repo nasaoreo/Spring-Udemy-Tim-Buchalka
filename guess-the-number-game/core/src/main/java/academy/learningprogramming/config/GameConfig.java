d2d03018d9c7b1ca901f76de9f71ceed6b66e5a3
nasaoreo <nasaoreo121@gmail.com>
2021-04-23 13:33:29 +0900

    Inject values of game configuration using properties  - add a game.properties file (games.properties)  - add an annotation to get values from properties (GameConfig.java)  - set default values of the game configuration (GameConfig.java)

--- guess-the-number-game/core/src/main/java/academy/learningprogramming/config/GameConfig.java
+++ guess-the-number-game/core/src/main/java/academy/learningprogramming/config/GameConfig.java
@@ -2,14 +2,20 @@ package academy.learningprogramming.config;
 
 import academy.learningprogramming.GuessCount;
 import academy.learningprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-    // == fiedls ==
-    private int maxNumber = 50;
-    private int guessCount = 5;
+    // == fields ==
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
