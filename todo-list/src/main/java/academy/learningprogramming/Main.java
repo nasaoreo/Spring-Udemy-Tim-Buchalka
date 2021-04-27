97d98ecd4916c13addf8ecda86d448e10f4e4e6a
nasaoreo <nasaoreo121@gmail.com>
2021-04-27 14:14:23 +0900

    Create Spring Web MVC Project

---
+++ todo-list/src/main/java/academy/learningprogramming/Main.java
@@ -0,0 +1,10 @@
+package academy.learningprogramming;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Hello, Maven");
+    }
+}
