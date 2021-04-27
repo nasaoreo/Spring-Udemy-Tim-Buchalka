f6cb0b1432fe69954260e4584207bfacca0af397
nasaoreo <nasaoreo121@gmail.com>
2021-04-27 18:57:44 +0900

    Create simple MVC demo code  - Setup the spring mvc dispatcher servlet using java configuration(WellAppInitializer.java)  - Create a controller class and controller methods mapped requests(DemoController.java)  - Create a view (welcome.jsp)    > use a ViewResolver object (WebConfig.java)

---
+++ todo-list/src/main/java/academy/learningprogramming/config/WellAppInitializer.java
@@ -0,0 +1,37 @@
+package academy.learningprogramming.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class WellAppInitializer implements WebApplicationInitializer {
+
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("onStartup");
+
+        // create the spring application context
+        AnnotationConfigWebApplicationContext conext =
+                new AnnotationConfigWebApplicationContext();
+        conext.register(WebConfig.class);
+
+        // create the dispatcher servlet
+        DispatcherServlet dispatcherServlet =
+                new DispatcherServlet(conext);
+
+        // register and configure the dispatcher servlet
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
