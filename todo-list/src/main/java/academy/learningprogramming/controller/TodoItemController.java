10a8bbf7dd0ecf6660e91f8223645097e90b914a
nasaoreo <nasaoreo121@gmail.com>
2021-05-04 19:04:44 +0900

    Implement update todo-item functionality  - add navigation to update the item in item_list.jsp(/view/item_list.jsp)  - update addEditItem(), processItem() handler methods to update item in TodoItemController.java(/controller/TodoItemController.java)

--- todo-list/src/main/java/academy/learningprogramming/controller/TodoItemController.java
+++ todo-list/src/main/java/academy/learningprogramming/controller/TodoItemController.java
@@ -45,8 +45,15 @@ public class TodoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") int id,
+                              Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+
+        if (todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
+
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -54,7 +61,13 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem form = {}", todoItem);
-        todoItemService.addItem(todoItem);
+
+        if (todoItem.getId() == 0) {
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
+
         return "redirect:/" + Mappings.ITEMS;
     }
 
