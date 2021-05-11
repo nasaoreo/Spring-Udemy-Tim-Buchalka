f0bcb2796060a6be62d8aec2c91478f1bfd3962a
nasaoreo <nasaoreo121@gmail.com>
2021-05-11 18:32:50 +0900

    Add home view using thymeleaf

---
+++ guess-the-number-game/web/src/main/java/academy/learningprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learningprogramming.util;
+
+public class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == costructors ==
+    private ViewNames() {
+    }
+}
