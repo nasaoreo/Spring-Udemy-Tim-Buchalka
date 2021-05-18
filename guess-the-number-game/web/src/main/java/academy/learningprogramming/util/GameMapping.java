4533389551ef4882b41c61684e4688a1e5d42509
nasaoreo <nasaoreo121@gmail.com>
2021-05-18 15:51:04 +0900

    Create a play page and GameController  - create a thymeleaf template for play page(web/.../resources/templates/play.html)  - create a GameController class and implement play-related request methods (web/.../controller/GameController.java)  - add view names to the ViewNames class(web/.../util/ViewNames.java)  - add util classes(web/.../util/AttributeNames.java, web/.../util/GameMapping.java)

--- guess-the-number-game/web/src/main/java/academy/learningprogramming/util/GameMapping.java
+++ guess-the-number-game/web/src/main/java/academy/learningprogramming/util/GameMapping.java
@@ -1,13 +1,13 @@
 package academy.learningprogramming.util;
 
-public class ViewNames {
+public class GameMapping {
 
     // == constants ==
     public static final String HOME = "home";
     public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
     public static final String GAME_OVER = "game-over";
 
-    // == costructors ==
-    private ViewNames() {
-    }
+    // == constructors ==
+    private GameMapping() {}
 }
