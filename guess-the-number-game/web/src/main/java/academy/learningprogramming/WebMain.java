c4f5701df757cb4e2b938a1b6183f6888c3306a3
nasaoreo <nasaoreo121@gmail.com>
2021-05-21 23:03:13 +0900

    set message internationalization and set the locale  - add messages.properties(core/.../resources/messages_*.properties)  - replace text in html files to the properties by using thymeleaf attribute(web/.../resources/*.html, web/.../resources/*.th.xml)  - set default locale and locale resolver(web/.../resources/application.properties)

--- guess-the-number-game/web/src/main/java/academy/learningprogramming/WebMain.java
+++ guess-the-number-game/web/src/main/java/academy/learningprogramming/WebMain.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebMain {
+
     public static void main(String[] args) {
         SpringApplication.run(WebMain.class, args);
     }
