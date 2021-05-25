791afffe04ce845a85b43b6b776dadfa4921b03b
nasaoreo <nasaoreo121@gmail.com>
2021-05-25 15:00:30 +0900

    Set Request Interceptor to handle requests  - create RequestInterceptor class and implement preHandle, postHandle, afterCompletion methods (web/.../java/interceptor/RequestInterceptor.java)  - add addInterceptors method to use Interceptor (web/.../java/config/WebConfig.java)

--- guess-the-number-game/web/src/main/java/academy/learningprogramming/config/WebConfig.java
+++ guess-the-number-game/web/src/main/java/academy/learningprogramming/config/WebConfig.java
@@ -1,7 +1,9 @@
 package academy.learningprogramming.config;
 
+import academy.learningprogramming.interceptor.RequestInterceptor;
 import academy.learningprogramming.util.ViewNames;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
         registry.addViewController("home").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
