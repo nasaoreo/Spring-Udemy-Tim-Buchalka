93807aa4f5e1d34be4f2ce13152069b728e61df1
nasaoreo <nasaoreo121@gmail.com>
2021-06-17 17:20:44 +0900

    Test code for IoC(Inversion of Control)

---
+++ spring_ioc_demo2/src/main/java/com/julie/IOCApp2.java
@@ -0,0 +1,24 @@
+package com.julie;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class IOCApp2 {
+    public static void main(String[] args) {
+        // ========== Spring IoC Container : Accessing metadata from the classpath ===================
+
+        // create the application context (container)
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        // create the bean
+        Organization org = (Organization) ctx.getBean("myorg");
+
+        // Invoke the company slogan via the bean
+        org.corporateSlogan();
+
+        // close the application context (container)
+        ((ClassPathXmlApplicationContext)ctx).close();
+
+        // ==============================================================================================
+    }
+}
---
+++ spring_ioc_demo2/src/main/java/com/julie/Organization.java
@@ -0,0 +1,8 @@
+package com.julie;
+
+public class Organization {
+    public void corporateSlogan(){
+        String slogan = "We build the ultimate driving machines!";
+        System.out.println(slogan);
+    }
+}
