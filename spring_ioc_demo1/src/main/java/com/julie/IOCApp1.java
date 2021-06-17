d71d68c96ca048acf035c1a806d0b8e6c6587fb4
nasaoreo <nasaoreo121@gmail.com>
2021-06-17 16:34:58 +0900

    Test code for IoC(Inversion of Control)

---
+++ spring_ioc_demo1/src/main/java/com/julie/IOCApp1.java
@@ -0,0 +1,20 @@
+package com.julie;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class IOCApp1 {
+    public static void main(String[] args) {
+        // create the application context (container)
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
+
+        // create the bean
+        Organization org = (Organization) ctx.getBean("myorg");
+
+        // Invoke the company slogan via the bean
+        org.corporateSlogan();
+
+        // close the application context (container)
+        ((FileSystemXmlApplicationContext)ctx).close();
+    }
+}
