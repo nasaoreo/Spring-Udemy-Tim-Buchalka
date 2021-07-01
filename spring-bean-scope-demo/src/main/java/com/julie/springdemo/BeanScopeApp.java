22d5b9030721f3574bba8fa13341d15d25f9f4ef
nasaoreo <nasaoreo121@gmail.com>
2021-07-01 17:26:41 +0900

    Demo project for testing spring bean scopes

---
+++ spring-bean-scope-demo/src/main/java/com/julie/springdemo/BeanScopeApp.java
@@ -0,0 +1,27 @@
+package com.julie.springdemo;
+
+import com.julie.springdemo.domain.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeApp {
+
+    public static void main(String[] args) {
+        //create the application context (context)
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-cp.xml");
+
+        //create the bean
+        Organization org = (Organization)ctx.getBean("myorg");
+
+        //print the company slogan
+        System.out.println(org.corporateSlogan());
+
+        //print organization details.
+        System.out.println(org);
+
+        //invoke corporateService via organization bean
+        System.out.println(org.corporateService());
+
+        ((ClassPathXmlApplicationContext)ctx).close();
+    }
+}
