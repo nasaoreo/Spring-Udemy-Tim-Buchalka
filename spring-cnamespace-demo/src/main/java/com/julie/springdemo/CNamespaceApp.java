2c2f80d327cf94d947cebb6bd2015d70da4987be
nasaoreo <nasaoreo121@gmail.com>
2021-07-06 21:35:03 +0900

    Demo project for spring bean definition with C/P namespace

---
+++ spring-cnamespace-demo/src/main/java/com/julie/springdemo/CNamespaceApp.java
@@ -0,0 +1,21 @@
+package com.julie.springdemo;
+
+import com.julie.springdemo.domain.Organization;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CNamespaceApp {
+
+    public static void main(String[] args) {
+        //create the application context (context)
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-cp.xml");
+
+        //create the bean
+        Organization org = (Organization)ctx.getBean("myorg");
+
+        //pring organization details.
+        System.out.println(org);
+
+        ((ClassPathXmlApplicationContext)ctx).close();
+    }
+}
