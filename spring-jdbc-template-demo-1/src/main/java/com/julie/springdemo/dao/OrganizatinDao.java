de2d37a479e6b439e4e9c3d75a540ea913cbddcf
nasaoreo <nasaoreo121@gmail.com>
2021-07-19 12:31:52 +0900

    Demo project for working with spring jdbc  - create Organization class in domain package and add get/set methods (main/java/.../domain/Organization.java)  - create OrganizationDao interface in dao package and add crud methods to communicate with db (main/java/.../dao/OrganizationDao.java)  - create OrganizationRowMapper class in daoimpl pacakge and implement RowMapper interface to generate Organization object from query execution results(main/java/.../daoimpl/OrganizationRowMapper.java)

---
+++ spring-jdbc-template-demo-1/src/main/java/com/julie/springdemo/dao/OrganizatinDao.java
@@ -0,0 +1,29 @@
+package com.julie.springdemo.dao;
+
+import com.julie.springdemo.domain.Organization;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public interface OrganizatinDao {
+
+    //Set the data-source that will be required to create a connection to the database.
+    public void setDataSource(DataSource dataSource);
+
+    //Create a record in the organization table.
+    public boolean create(Organization org);
+
+    //Retrieve a single organization.
+    public Organization getOrganization(Integer id);
+
+    //Retrieve all organizations from the table.
+    public List<Organization> getAllOrganizatios();
+
+    //Delete a specific organization from the table.
+    public boolean delete(Organization org);
+
+    //Update an existing orgazniation.
+    public boolean update(Organization org);
+
+    public void cleanUp();
+}
